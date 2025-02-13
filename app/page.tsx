df2a31261c24e57a9a64326aab17761d5f60c8f9
nishantsingh604 <nishantrohilla28@gmail.com>
2025-02-13 23:22:59 +0530

    Created and Completed AuthForm

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import {Button} from "@/components/ui/button"
 import BookOverview from "@/components/BookOverview";
 import BookList from "@/components/BookList";
+import { sampleBooks } from "./constants";
 
 const Home = () => (
     <>
