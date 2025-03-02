014f7d6371b98f3e3cd01be8fa2cb23fd8efca8e
nishantsingh604 <nishantrohilla28@gmail.com>
2025-03-02 21:03:14 +0530

    Fixed toast component bugs, added profile page with icon configuration, improved authentication, integrated Auth.ts, and implemented session/token validation.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,55 +4,67 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import localFont from "next/font/local";
 import { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
+import { Session } from "inspector/promises";
+import { auth } from "@/auth";
 
 const ibmPlexSans = localFont({
-  src:[
+  src: [
     {
-      path: '/fonts/IBMPlexSans-Regular.ttf', weight:'400', style:'normal'
+      path: "/fonts/IBMPlexSans-Regular.ttf",
+      weight: "400",
+      style: "normal"
     },
     {
-      path: '/fonts/IBMPlexSans-Medium.ttf', weight:'500', style:'normal'
+      path: "/fonts/IBMPlexSans-Medium.ttf",
+      weight: "500",
+      style: "normal"
     },
     {
-      path: '/fonts/IBMPlexSans-SemiBold.ttf', weight:'600', style:'normal'
+      path: "/fonts/IBMPlexSans-SemiBold.ttf",
+      weight: "600",
+      style: "normal"
     },
     {
-      path: '/fonts/IBMPlexSans-Bold.ttf', weight:'700', style:'normal'
-    },
-  ],
+      path: "/fonts/IBMPlexSans-Bold.ttf",
+      weight: "700",
+      style: "normal"
+    }
+  ]
 });
 
 const bebasNeue = localFont({
-  src:[{path: './fonts/BebasNeue-Regular.ttf', weight:"400", style:"normal"},],
-  variable:"--bebas-neue",
-})
+  src: [
+    { path: "./fonts/BebasNeue-Regular.ttf", weight: "400", style: "normal" }
+  ],
+  variable: "--bebas-neue"
+});
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  subsets: ["latin"],
+  subsets: ["latin"]
 });
 
 export const metadata: Metadata = {
   title: "Panne",
-  description: "Inn Panno mai hi sab hai...",
+  description: "Inn Panno mai hi sab hai..."
 };
 
-const RootLayout = ({
-  children,
-}: {
-  children: React.ReactNode;
-}) => {
+const RootLayout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await auth();
   return (
     <html lang="en">
-      <body
-        className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
-      >
-        {children}
+      <SessionProvider session={session}>
+        <body
+          className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
+        >
+          {children}
 
-        <Toaster />
-      </body>
+          <Toaster />
+        </body>
+      </SessionProvider>
     </html>
   );
-}
+};
 
 export default RootLayout;
